e4207af23a40dcb4662bdbdb19c31d35ec5abc24
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2024-12-14 13:50:50 +0100

    Wavlo/Typing.JS v2.0 - new version of Typing.JS copied from Wavlo to repository

--- type.js
+++ type.js
@@ -1,18 +1,39 @@
-function typeText(parentElement, text, callback) {
-    let index = 0;
-    const span = document.createElement('span');
-    span.classList.add('typing-js-span');
-    parentElement.appendChild(p);
+function typeText(parentElement, texts, callback) {
+        let currentTextIndex = 0;
 
-    function type() {
-        if (index < text.length) {
-            p.innerHTML += text[index];
-            index++;
-            setTimeout(type, 50);
-        } else if (callback) {
-            return callback();
+        function type() {
+            if (currentTextIndex >= texts.length) {
+            if (callback) {
+                return callback();
+            }
+            return;
         }
+
+        let text = texts[currentTextIndex];
+        let index = 0;
+
+        function clearAndType() {
+            if (parentElement.innerHTML.length > 0) {
+                parentElement.innerHTML = parentElement.innerHTML.slice(0, -1);
+                setTimeout(clearAndType, 20);
+            } else {
+                typeCharacter();
+            }
+        }
+
+        function typeCharacter() {
+            if (index < text.length) {
+                parentElement.innerHTML += text[index];
+                index++;
+                setTimeout(typeCharacter, 100);
+            } else {
+                currentTextIndex++;
+                setTimeout(type, 500);
+            }
+        }
+
+        clearAndType();
     }
-    
+
     type();
 }
